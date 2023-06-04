b501ff759f2a82f6e36ea43563c03556c4fd6b40
DESKTOP-OSHDLBA\ADMIN <varsha.khichade@itgurussoftware.com>
2023-06-04 18:09:38 +0530

    Igniting App

--- App.js
+++ App.js
@@ -1,3 +1,20 @@
+import { useEffect } from 'react';
+
+export function MyComponent() {
+    useEffect(() => {
+        const subscription = someAPI.subscribe(data => {
+            // do something with the data
+        });
+        return () => {
+            subscription.unsubscribe();
+        }
+    }, []);
+    return <div>...</div>;
+}
+
+
+import React from "react";
+import ReactDOM from "react-dom/client";
 // const heading = React.createElement(
 //   "h1",
 //   { id: "headings" },
@@ -8,21 +25,17 @@
 // root.render(heading);
 //render:: converts object in to heading tag and put into DOM
 
-
 // Nested structure using react
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
- [ React.createElement(
-    "div",
-    { id: "child" },
-    [React.createElement("h1", {}, "This is h1 nested"),React.createElement("h2", {}, "This is h2 nested")]
-  ), React.createElement(
-    "div",
-    { id: "child2" },
-    [React.createElement("h1", {}, "This is h1 child2 nested"),React.createElement("h2", {}, "This is h2 child 2 nested")]
-  )]
-);
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "This is Namste React"),
+    React.createElement("h2", {}, "This is h2 nested"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "This is h1 child2 nested"),
+    React.createElement("h2", {}, "This is h2 child 2 nested"),
+  ]),
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
--- READMe.md
+++ READMe.md
@@ -1,1 +1,17 @@
 #React using CDN
+
+#Parcel creates
+ -Dev build
+ -local server
+ -Reload or refresh Our page Automatically (HMR-Hot module replacement)
+ -FIle Watching algorithm -writtern in C++
+ -caching-faster builds
+ -Image Optimization
+ -Minification
+ -Bundling
+ -Compressing
+ -Consistant Hashing
+ -Code splitting
+ -Error Handling
+ -Diagnostics
+ -Tree shaking-Removes Unused Code
